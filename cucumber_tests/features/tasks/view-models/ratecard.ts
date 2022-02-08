a9cc53f6433cfdacd58dc64347dccb2b274961b8
mimran1989 <imran@provusinc.com>
2022-02-08 19:42:04 +0530

    updated

---
+++ cucumber_tests/features/tasks/view-models/ratecard.ts
@@ -0,0 +1,30 @@
+/*import { Select, WebComponent } from '../../support/componentSelector';
+
+const COMPONENT = WebComponent('records-modal-lwc-detail-panel-wrapper');
+
+const ratecardpage = {
+	
+    ratecardname : COMPONENT.findWebComponent('records-lwc-detail-panel').findWebComponent('records-base-record-form').findWebComponent('records-lwc-record-layout').
+    child(0).findWebComponent('force-record-layout-base-input').findWebComponent('lightning-input').child('label').withExactText('Rate Card Name').sibling(0).child('input')
+    usernameInput: Select('input').withAttribute('id', 'username'),
+	passwordInput: Select('input').withAttribute('id', 'password'),
+	logInButton: Select('input').withAttribute('id', 'Login'),
+};
+
+export default ratecardpage;
+
+document.querySelector('one-appnav').
+shadowRoot.querySelector('one-app-nav-bar').
+shadowRoot.querySelectorAll('one-app-nav-bar-item-root')[7].
+shadowRoot.querySelector('span.slds-truncate')"
+
+
+
+
+document.querySelector('records-modal-lwc-detail-panel-wrapper').
+shadowRoot.querySelector('records-lwc-detail-panel').
+shadowRoot.querySelector('records-base-record-form').
+shadowRoot.querySelector('records-lwc-record-layout').
+shadowRoot.querySelector('forcegenerated-detailpanel_psq__ratecard__c___012000000000000aaa___full___create___recordlayout2').
+shadowRoot.querySelector('force-record-layout-base-input').
+shadowRoot.querySelector('lightning-input').shadowRoot.querySelector('input.slds-input')"*/
